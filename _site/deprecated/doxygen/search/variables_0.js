779641983099559087fe9aeef67961402aa85ddc
wonjongbot <wonjongbot@gmail.com>
2022-08-16 13:22:10 -0500

    local site testing

---
+++ _site/deprecated/doxygen/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['malformed_0',['malformed',['../a00022.html#a04ac5dc3b6e625ab3ab26a0e17249c4b',1,'pktlab_uri']]]
+];
---
+++ _site/deprecated/workers/fetch-data.worker.js
@@ -0,0 +1,73 @@
+self.addEventListener("message",function(e){
+    const data = e.data;
+    if(data.status=="load" && data.url!=null){
+        const dataPromises = [];
+        const dataKeys = Object.keys(data.url);
+        dataKeys.forEach((key)=>{
+            dataPromises.push(fetchDataFromURL(data.url[key]));
+        });
+        Promise.all(dataPromises).then((data)=>{
+            const aggData = {};
+            data.forEach((result, i)=>{
+                aggData[dataKeys[i]] = result;
+            })
+            self.postMessage({
+                "status":"finished",
+                "message":aggData
+            })
+        })
+    }else{
+        self.postMessage({
+            "status":"error",
+            "message":"Invalid arguments"
+        })
+    }
+})
+
+function fetchDataFromURL(source){
+    console.time("Worker Data Load"+source.url)
+    const url = source.url;
+    const format = source.format;
+    const decoder = new TextDecoder('utf-8');
+    const promise = new Promise((resolve,reject)=>{
+        fetch(url).then(async (response)=>{
+            if(response.ok){
+                if(format=="jsonl"){
+                    const jsonRows = [];
+                    let bufferString = "";
+                    const reader = response.body.getReader();
+                    let readDone = false;
+                    do{
+                        await reader.read().then(({value,done})=>{
+                            readDone = done;
+                            const chunk = decoder.decode(value);
+                            const chunkLength = chunk.length;
+                            for(let i=0;i<chunkLength;i++){
+                                const currentChar = chunk.charAt(i);
+                                if(currentChar=="\n"){
+                                    jsonRows.push(JSON.parse(bufferString));
+                                    bufferString="";
+                                }else{
+                                    bufferString+=currentChar;
+                                }
+                            }
+                        })
+                    }while(!readDone);
+                    if(bufferString.length>0){
+                        jsonRows.push(JSON.parse(bufferString));
+                    }
+                    resolve(jsonRows);
+                    console.timeEnd("Worker Data Load"+source.url)
+                }else if(format=="json"){
+                    resolve(JSON.parse(response.text()))
+                }
+                
+            }else{
+                throw Error("Invalid response");
+            }
+        }).catch((error)=>{
+            reject(error);
+        });
+    });
+    return promise;
+}
